b2ab1f278c256325e6bc2095747ad120e6c210fb
Vicheak <suonvicheak991@gmail.com>
2024-05-05 08:34:32 +0700

    Initial Commit

---
+++ AutoLayoutWithProgramming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutWithProgramming
+//
+//  Created by @suonvicheakdev on 5/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
